34e972c933d01261735770758bc558b280e139f8
alliespeer <alliespeer13@gmail.com>
2018-07-24 16:48:37 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -3,6 +3,7 @@ def Gangnam_style
   puts "1. Plant legs far apart, bend knees slightly and keep posture loose"
   puts "Base Footwork"
   puts "1. Lift right foot"
+  puts "2. Return right foot to the ground"
   puts 
 
 # Code your methods below
