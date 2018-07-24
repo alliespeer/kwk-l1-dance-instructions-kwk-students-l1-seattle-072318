efa859bfdc11c2df05953819b6bec2850621986e
alliespeer <alliespeer13@gmail.com>
2018-07-24 16:47:54 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,6 +1,6 @@
 def Gangnam_style
   puts "assume the proper stance"
-  puts "Plant legs far apart, bend knees slightly and keep posture loose"
+  puts "1. Plant legs far apart, bend knees slightly and keep posture loose"
   puts "Base Footwork"
   puts "1. Lift right foot"
   puts 
